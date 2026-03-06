93744709f0d301aa42a44d36fbdace2f3785863c
Vedant atugade <vedantatugade@gmail.com>
2026-03-06 20:12:27 +0000

    upadtes all

---
+++ Jenkinsfile
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: 'aws-credentials-1',
+                    usernameVariable: 'AWS_ACCESS_KEY_ID',
+                    passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                )]) {
+                    sh 'terraform apply -auto-approve'
+                }
+            }
+        }
+
+        stage('Get EC2 IP') {
+            steps {
+                sh 'terraform output -raw ec2_ip > ip.txt'
+            }
+        }
+
+        stage('Create Inventory') {
+            steps {
+                sh '''
+                echo "[web]" > inventory.ini
+                IP=$(cat ip.txt)
+                echo "$IP ansible_user=ec2-user ansible_ssh_private_key_file=small-task.pem" >> inventory.ini
+                '''
+            }
+        }
+
+        stage('Run Ansible Playbook') {
+            steps {
+                sh 'ansible-playbook -i inventory.ini nginx.yaml'
+            }
+        }
+
+    }
+}
---
+++ main.tf
@@ -0,0 +1,107 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create VPC
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "demo_subnet" {
+  vpc_id                  = aws_vpc.demo_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  tags = {
+    Name = "devops-gateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "devops-route-table"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.demo_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name   = "web-security-group"
+  vpc_id = aws_vpc.demo_vpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-0f3caa1cf4417e51b"
+  instance_type = "t3.micro"
+  key_name      = "small-task"
+
+  subnet_id              = aws_subnet.demo_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "devops-demo-instance"
+  }
+}
+
+# Output EC2 Public IP
+output "ec2_ip" {
+  value = aws_instance.web.public_ip
+}
